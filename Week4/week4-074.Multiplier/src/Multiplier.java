ac7e0a06919b0711ed680aadef107348b4790ee1
Andrei RWRZ <eraserk19@gmail.com>
2015-10-31 13:19:55 +0200

    74-75

---
+++ Week4/week4-074.Multiplier/src/Multiplier.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andrew
+ */
+public class Multiplier {
+    
+    private int number1;
+    
+    public Multiplier(int number) {
+        this.number1 = number;
+    }
+    
+    public int multiply(int otherNumber) {
+        return number1 * otherNumber;
+    }
+    
+}
